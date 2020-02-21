6c4b03d73851ab831c88e888fd7710001de6e995
Dant <antohaxor698@ya.ru>
2020-02-21 09:42:15 +0300

    Commit 1st step

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php 
+$localhost = 'C:\Users\Dant\Downloads';
+
+
+function scanf($dir) {
+    $d = opendir($dir);
+    while( false !== ($name = readdir($d)) ) {
+        if ( $name == '.' or $name == '..') continue;
+        if ( is_dir( $dir . '/'. $name) ) {
+
+            scanf($dir . '/'. $name);
+        } else
+
+            if ( pathinfo($name)['extension'] == "php") {
+                echo $name . "<br>";
+            }
+
+    }
+    closedir($d);
+}
+scanf($localhost);
+
+?>
