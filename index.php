af04e81da2a174cf77920c9efe3a55cd6482baf7
cherep721 <alexnorin721@gmail.com>
2020-03-17 21:53:29 +0300

    check homework

--- index.php
+++ index.php
@@ -1,24 +1,30 @@
-<?php 
+<?php
+
 $localhost = 'C:\Users\Dant\Downloads';
 
-
-function scanf($dir) {
-    $d = opendir($dir);
-    while( false !== ($name = readdir($d)) ) {
-        if ( $name == '.' or $name == '..') continue;
-        if ( is_dir( $dir . '/'. $name) ) {
-
-            scanf($dir . '/'. $name);
-        } else
-
-            if ( pathinfo($name)['extension'] == "php") {
-                echo $name . "<br>";
-            }
-
+/**
+ * @param string $dir
+ */
+function scanf(string $dir): void
+{
+    if (!$dirHandle = opendir($dir)) {
+        echo 'Fucking dir - ' . $dir;
+        return;
     }
-    closedir($d);
-}
-scanf($localhost);
 
-/*I have trouble??!
-?>
+    while ($name = readdir($dirHandle)) {
+        if ($name == '.' || $name == '..') {
+            continue;
+        }
+        if (is_dir($dir . '/' . $name)) {
+            scanf($dir . '/' . $name);
+            continue;
+        }
+        if (pathinfo($name)['extension'] === "php") {
+            echo $name . "<br>";
+        }
+    }
+    closedir($dirHandle);
+}
+
+scanf($localhost);
